c75a1cff89effb8904b438025f9bb82ed35eeae7
Dmitrii V. Lystsov <dlystsov@softmedialab.com>
2023-05-04 16:07:26 +0500

    change response

--- Diary.Api/Program.cs
+++ Diary.Api/Program.cs
@@ -1,6 +1,15 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+WebApplication app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// 1
+// app.MapGet("/", () => "Hello World!");
+// 2
+app.UseWelcomePage();
+// 3
+/* app.Run(async (context) =>
+{
+    await context.Response.WriteAsync("---");
+});
+*/
 
 app.Run();
