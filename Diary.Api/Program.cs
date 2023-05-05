d9edc0b0bb75232ac2d47a51d7a829f0e00e3199
Dmitrii V. Lystsov <dlystsov@softmedialab.com>
2023-05-05 12:34:41 +0500

    use Use

--- Diary.Api/Program.cs
+++ Diary.Api/Program.cs
@@ -3,13 +3,34 @@ WebApplication app = builder.Build();
 
 // 1
 // app.MapGet("/", () => "Hello World!");
+
 // 2
-app.UseWelcomePage();
+// app.UseWelcomePage();
+
 // 3
 /* app.Run(async (context) =>
 {
     await context.Response.WriteAsync("---");
 });
 */
+
+// 4
+string DateAsString = "";
+app.Use(async (context, next) =>
+{
+    Console.WriteLine("4: begin");
+    DateAsString = DateTime.Now.ToShortDateString();
+    // до передачи обработки следующему
+    await next.Invoke();
+    // после передачи обработки следующему
+    Console.WriteLine("4: end");
+});
+// терминальный middleware
+app.Run(async context =>
+{
+    Console.WriteLine("5: run");
+    await context.Response.WriteAsync($"Date: {DateAsString}");
+    // Console.WriteLine("5: begin"); // зависает если оставить!
+});
 
 app.Run();
